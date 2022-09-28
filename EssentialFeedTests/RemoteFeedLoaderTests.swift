9e9df6c1a382003d897563b0aec25e4cfbc87e80
Aleksandr Kornjushko <sasa330@gmail.com>
2022-09-28 14:43:57 +0300

    Remove unnecessary code in helper method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -58,13 +58,9 @@ class RemoteFeedLoaderTests: XCTestCase {
 
   private class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
-    var error: Error?
     var completions = [(Error) -> Void]()
 
     func get(from url: URL, completion: @escaping (Error) -> Void) {
-      if let error = error {
-        completion(error)
-      }
       completions.append(completion)
       requestedURLs.append(url)
     }

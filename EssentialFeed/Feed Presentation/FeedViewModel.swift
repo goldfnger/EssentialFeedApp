551f0070960b9eb9b0f86911131b34291ac6c22c
Aleksandr Kornjushko <sasa330@gmail.com>
2022-11-21 14:51:15 +0200

    Move "FeedViewModel" to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Aleksandr Kornjushko on 21.11.2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+  public var feed: [FeedImage]
+}

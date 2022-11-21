6399439314edab1add21b43dfe6354e9175c297d
Aleksandr Kornjushko <sasa330@gmail.com>
2022-11-21 14:52:08 +0200

    Move "FeedLoadingViewModel" to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Aleksandr Kornjushko on 21.11.2022.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+  public let isLoading: Bool
+}

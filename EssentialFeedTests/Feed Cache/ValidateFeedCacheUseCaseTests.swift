4ce9bd344dd6bd8c601c98ba5eca09086ce90d65
Aleksandr Kornjushko <sasa330@gmail.com>
2022-11-24 17:37:02 +0200

    Load "NSManagedObjectModel" instance lazily and cache it to prevent multiple "NSEntityDescriptions" claiming the same "NSManagedObject" model subclasses (This problem just generates warnings but could lead to undefined behavior in the future).

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -115,19 +115,19 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
     let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
     let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
     let deletionError = anyNSError()
-    
+
     expect(sut, toCompleteWith: .failure(deletionError), when: {
       store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
       store.completeDeletion(with: deletionError)
     })
   }
-  
+
   func test_validateCache_succeedsOnSuccessfulDeletionOfExpiredCache() {
     let feed = uniqueImageFeed()
     let fixedCurrentDate = Date()
     let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
     let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
-    
+
     expect(sut, toCompleteWith: .success(()), when: {
       store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
       store.completeDeletionSuccessfully()

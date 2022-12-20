02123c5955e8047639301e8bc51185d17cfbedc0
Aleksandr Kornjushko <sasa330@gmail.com>
2022-12-20 14:22:37 +0200

    Replace FeedPresenter with generic presenter

--- EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -12,14 +12,15 @@ import EssentialFeediOS
 final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
   private let feedLoader: () -> AnyPublisher<[FeedImage], Error>
   private var cancellable: Cancellable?
-  var presenter: FeedPresenter?
+  // generic presenter should match the Resource type array of FeedImage and view type is FeedViewAdapter(which holds logic of view to display feed)
+  var presenter: LoadResourcePresenter<[FeedImage], FeedViewAdapter>?
 
   init(feedLoader: @escaping() -> AnyPublisher<[FeedImage], Error>) {
     self.feedLoader = feedLoader
   }
 
   func didRequestFeedRefresh() {
-    presenter?.didStartLoadingFeed()
+    presenter?.didStartLoading()
 
     // sink is used to subscribe to the 'Publisher' and start the operation.
     // we need to hold(cancellable = ...) the result of the subscription which is 'Cancellable'. if we dont hold the Cancellable it will be deallocated, if it will be deallocated then it cancels the whole subscription.
@@ -32,13 +33,13 @@ final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
             // we do nothing
           case .finished: break
             
-            // if error occures
+            // if error occurs
           case let .failure(error):
-            self?.presenter?.didFinishLoadingFeed(with: error)
+            self?.presenter?.didFinishLoading(with: error)
           }
           // success value
         }, receiveValue: { [weak self] feed in
-          self?.presenter?.didFinishLoadingFeed(with: feed)
+          self?.presenter?.didFinishLoading(with: feed)
         })
   }
 }

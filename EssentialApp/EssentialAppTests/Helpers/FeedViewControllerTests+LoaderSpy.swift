c393573cf627e8dae0b47743196a10a7da37cf90
Aleksandr Kornjushko <sasa330@gmail.com>
2023-01-10 15:45:46 +0200

    Group "Load more" tests

--- EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -16,14 +16,9 @@ extension FeedUIIntegrationTests {
 
     //MARK: - FeedLoader
     private var feedRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
-    private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
 
     var loadFeedCallCount: Int {
       return feedRequests.count
-    }
-
-    var loadMoreCallCount: Int {
-      return loadMoreRequests.count
     }
 
     func loadPublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
@@ -32,19 +27,30 @@ extension FeedUIIntegrationTests {
       return publisher.eraseToAnyPublisher()
     }
 
+    func completeFeedLoadingWithError(at index: Int = 0) {
+      feedRequests[index].send(completion: .failure(anyNSError()))
+    }
+
     func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
       feedRequests[index].send(Paginated(items: feed, loadMorePublisher: { [weak self] in
-        // now instead of counting we need to create a publishers
-        // because it is expect a 'closure', but we are returning 'publishers' so we need to create a 'bridge'
-        let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
-        self?.loadMoreRequests.append(publisher)
-        return publisher.eraseToAnyPublisher()
+        self?.loadMorePublisher() ?? Empty().eraseToAnyPublisher()
       }))
     }
 
-    func completeFeedLoadingWithError(at index: Int = 0) {
-      let error = NSError(domain: "an error", code: 0)
-      feedRequests[index].send(completion: .failure(error))
+    //MARK: - LoadMoreFeedLoader
+
+    private var loadMoreRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
+
+    var loadMoreCallCount: Int {
+      return loadMoreRequests.count
+    }
+
+    func loadMorePublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
+      // now instead of counting we need to create a publishers
+      // because it is expect a 'closure', but we are returning 'publishers' so we need to create a 'bridge'
+      let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
+      loadMoreRequests.append(publisher)
+      return publisher.eraseToAnyPublisher()
     }
 
     // when we completing more we are going to
@@ -54,15 +60,12 @@ extension FeedUIIntegrationTests {
         items: feed,
         // if it is last page(true) we return nil, we dont have more items to load, otherwise we create a new publisher
         loadMorePublisher: lastPage ? nil : { [weak self] in
-          let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
-          self?.loadMoreRequests.append(publisher)
-          return publisher.eraseToAnyPublisher()
+          self?.loadMorePublisher() ?? Empty().eraseToAnyPublisher()
       }))
     }
 
     func completeLoadMoreWithError(at index: Int = 0) {
-      let error = NSError(domain: "an error", code: 0)
-      loadMoreRequests[index].send(completion: .failure(error))
+      loadMoreRequests[index].send(completion: .failure(anyNSError()))
     }
 
     //MARK: - FeedImageDataLoader
@@ -91,8 +94,7 @@ extension FeedUIIntegrationTests {
     }
 
     func completeImageLoadingWithError(at index: Int = 0) {
-      let error = NSError(domain: "an error", code: 0)
-      imageRequests[index].completion(.failure(error))
+      imageRequests[index].completion(.failure(anyNSError()))
     }
   }
 }

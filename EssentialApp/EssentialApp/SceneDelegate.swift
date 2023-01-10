62876a9a5412d249887660e39919db575aa5a10e
Aleksandr Kornjushko <sasa330@gmail.com>
2023-01-10 13:50:47 +0200

    Fetch current items from cache when needed instead of keeping them in memory all the time

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -93,15 +93,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   }
 
   // we need to call this method recursively
-  private func makeRemoteLoadMoreLoader(items: [FeedImage], last: FeedImage?) -> AnyPublisher<Paginated<FeedImage>, Error> {
+  // not to keeping items in memory
+  private func makeRemoteLoadMoreLoader(last: FeedImage?) -> AnyPublisher<Paginated<FeedImage>, Error> {
     // makeRemoteLoadMoreLoader it creates a remote feed loader after the last item
-    makeRemoteFeedLoader(after: last)
-    // it gets the new items
-      .map { newItems in
+    // 1)) load first from the cache
+    localFeedLoader.loadPublisher()
+    // 7) another way to do mentioned steps is to use 'zip publisher'
+    // 2)) and then from the feed loader
+      .zip(makeRemoteFeedLoader(after: last))
+//    // every time we need a new 'items' we can load them from the cache
+//    // we receive newItems from 'makeRemoteFeedLoader' after last item
+//    // 2) then we load the cache
+//      .flatMap({ [localFeedLoader] newItems in
+//        // load publisher from the local feed loader which is the cache and 'map' 'cachedItems' that we receive from the 'localFeedLoader'
+//        // 3) we get 'result' of the 'newItems' and the 'cache'
+//        localFeedLoader.loadPublisher().map { cachedItems in
+//          // and here we can return 'newItems'
+//          // 4) and we combine them into a tuple
+//          (newItems, cachedItems)
+//        }
+//      })
+//    // it gets the new items
+//    // 5) we are going to have a tuple of 'newItems'
+      .map { (cachedItems, newItems) in
         // and combine
         // we append the 'lastItems(items)' with the 'newItems'
         // and keep appending all the elements until 'newItems' is empty which means we reached the end of the pagination
-        (items + newItems, newItems.last)
+        // 6) and we can combine 'cachedItems' with 'newItems'
+        (cachedItems + newItems, newItems.last)
         // then makes a page
       }.map(makePage)
     // only for testing purpose
@@ -137,7 +156,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     Paginated(items: items,
               // when loading a new page last items is a 'newItems.last', because 'lastItem' can be empty and if 'newItems' is empty it means we reached the end
               loadMorePublisher: last.map { last in
-      { self.makeRemoteLoadMoreLoader(items: items, last: last) }
+      { self.makeRemoteLoadMoreLoader(last: last) }
     })
   }
 

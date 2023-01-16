7baefe01756d9bd8435aa7916528c0351814aa5f
Aleksandr Kornjushko <sasa330@gmail.com>
2023-01-16 14:34:09 +0200

    Optimize CoreData implementation to reduce cache misses

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -18,7 +18,13 @@ final class ManagedFeedImage: NSManagedObject {
 }
 
 extension ManagedFeedImage {
-  static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data?{
+  // when loading data as well as we load the cells
+  static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
+    // we try to look for the in-memory 'temporary lookup' for data for the URL and use it instead of making database request which is much slower,
+    // because 'dictionary lookup' is constant time which is much faster then a DB query.
+    // will return 'data' 'immediately', but if not then  'first()' request will be fired.
+    if let data = context.userInfo[url] as? Data { return data }
+
     return try first(with: url, in: context)?.data
   }
   
@@ -31,17 +37,32 @@ extension ManagedFeedImage {
   }
 
   static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-    return NSOrderedSet(array: localFeed.map { local in
+    // first we create the new collection of images
+    let images = NSOrderedSet(array: localFeed.map { local in
       let managed = ManagedFeedImage(context: context)
       managed.id = local.id
       managed.imageDescription = local.description
       managed.location = local.location
       managed.url = local.url
+      // when we are creating new images we can try to find data for that URL in the 'temporary' cache
+      managed.data = context.userInfo[local.url] as? Data
       return managed
     })
+    // then clear 'temporary lookup' data
+    context.userInfo.removeAllObjects()
+    return images
   }
 
   var local: LocalFeedImage {
     return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+  }
+
+  // invoked right before deletion
+  override func prepareForDeletion() {
+    super.prepareForDeletion()
+
+    // just a 'temporary lookup' 'dictionary' which is not persisted to disk
+    // we have data 'temporary' in the 'userInfo' dictionary
+    managedObjectContext?.userInfo[url] = data
   }
 }

771b4ead000f2fdfd49c36d3547176b7083e29af
Aleksandr Kornjushko <sasa330@gmail.com>
2023-01-20 14:21:41 +0200

    Make NullStore FeedStore implementation sync

--- EssentialApp/EssentialApp/NullStore.swift
+++ EssentialApp/EssentialApp/NullStore.swift
@@ -10,19 +10,14 @@ import EssentialFeed
 // Null Object pattern - complete the operation with the simplest null operation, no result
 // it will provide a 'default neutral behaviour' for 'store' if application gets in a weird/bug state. this 'default neutral behaviour' better to do the minimum possible!
 class NullStore: FeedStore & FeedImageDataStore {
-  func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+  func deleteCachedFeed() throws {
     // simply complete the operation so the clients wont be hanging forever
     // different 'use cases' may required different behaviours to complete, but for our cases success is the case.
-    completion(.success(()))
   }
 
-  func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-    completion(.success(()))
-  }
+  func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date) throws {}
 
-  func retrieve(completion: @escaping RetrievalCompletions) {
-    completion(.success(.none))
-  }
+  func retrieve() throws -> CachedFeed? { .none }
 
   func insert(_ data: Data, for url: URL) throws {}
 
